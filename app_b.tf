9c9557888b295cd8d32fe79ca5cf2e100004a3d5
Ryan Rousseau <r@ryanrousseau.com>
2024-03-18 11:07:30 -0500

    Update app_b tenant vars

--- app_b.tf
+++ app_b.tf
@@ -1,4 +1,5 @@
 resource "octopusdeploy_project" "app_b" {
+  name                                 = "App B"
   auto_create_release                  = false
   default_guided_failure_mode          = "EnvironmentDefault"
   default_to_skip_if_already_installed = false
@@ -8,7 +9,6 @@ resource "octopusdeploy_project" "app_b" {
   is_discrete_channel_release          = false
   is_version_controlled                = false
   lifecycle_id                         = data.octopusdeploy_lifecycles.default.lifecycles[0].id
-  name                                 = "App B"
   project_group_id                     = data.octopusdeploy_project_groups.cloud-team.project_groups[0].id
   tenanted_deployment_participation    = "Tenanted"
 
@@ -25,10 +25,40 @@ resource "octopusdeploy_project" "app_b" {
   }
 
   template {
-    default_value = "example-default-value"
-    help_text     = "example-help-test"
-    label         = "example-label"
-    name          = "example-template-value"
+    default_value = "Accounts-2923"
+    help_text     = "The AWS account used by this account, cluster, application combination."
+    label         = "AWS Account"
+    name          = "Project.Tenant.AWS.Account"
+    display_settings = {
+      "Octopus.ControlType" : "AmazonWebServicesAccount"
+    }
+  }
+
+  template {
+    default_value = "us-east-2"
+    help_text     = "The region code for AWS resources used by this account and cluster."
+    label         = "AWS Region Code"
+    name          = "Project.Tenant.AWS.RegionCode"
+    display_settings = {
+      "Octopus.ControlType" : "SingleLineText"
+    }
+  }
+  
+  template {
+    default_value = "##{Octopus.Deployment.Tenant.Name | ToLower | Replace \" \" \"-\" }-##{Octopus.Project.Name  | ToLower | Replace \" \" \"-\" }-stack"
+    help_text     = "The name of the CloudFOrmation stackt used by this account, cluster, application combination."
+    label         = "AWS CloudFormation Stack Name"
+    name          = "Project.Tenant.CloudFormation.StackName"
+    display_settings = {
+      "Octopus.ControlType" : "SingleLineText"
+    }
+  }
+
+  template {
+    default_value = "##{Octopus.Deployment.Tenant.Name | ToLower | Replace \" \" \"-\" }-##{Octopus.Project.Name  | ToLower | Replace \" \" \"-\" }-bucket"
+    help_text     = "The name of the S3 bucket used by this account, cluster, application combination."
+    label         = "AWS S3 Bucket Name"
+    name          = "Project.Tenant.CloudFormation.AppBucketName"
     display_settings = {
       "Octopus.ControlType" : "SingleLineText"
     }
